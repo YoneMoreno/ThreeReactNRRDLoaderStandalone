60d2162aca76c34c4fc2240243506687d35ae3ed
Yone <yone.moreno101@alu.ulpgc.es>
2018-03-09 13:15:59 +0000

    TODO: learn how to make a global variable in webpack to use NRRDLoader

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as THREE from 'three';
+
+window.THREE = THREE;
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
